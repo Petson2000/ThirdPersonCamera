3421a6bc7ff12008a7a2cbeeca0a85017d3f424e
Petson2000 <erik.pettersson@futuregames.nu>
2020-03-04 16:37:10 +0100

    Tweaks to playermovement, turn with camera angles.

--- ThirdPersonCamera/Assets/Scripts/Player.cs
+++ ThirdPersonCamera/Assets/Scripts/Player.cs
@@ -4,59 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    [Tooltip("Assign Camera Here")]
-    Transform playerInputSpace = default;
-
-    [SerializeField, Range(0f, 100f)]
+	[SerializeField, Range(0f, 100f)]
     float maxSpeed = 10f;
 
     [SerializeField, Range(0f, 100f)]
     float maxAcceleration = 10f;
 
-    Vector3 desiredVelocity;
+    [SerializeField]
+    Transform playerInputSpace = default;
 
     Vector3 velocity;
 
-    void Start()
-    {
-        
-    }
+    Vector3 desiredVelocity;
 
     void Update()
     {
         Vector2 playerInput;
-
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
-
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-
-        
-        float maxSpeedChange = maxAcceleration * Time.deltaTime;
-
-        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-        velocity.z = Mathf.MoveTowards(velocity.x, desiredVelocity.z, maxSpeedChange);
 
         if(playerInputSpace)
         {
-            Vector3 forward = playerInputSpace.forward;
-            forward.y = 0f;
-            forward.Normalize();
-            Vector3 right = playerInputSpace.right;
-            right.y = 0f;
-            right.Normalize();
-
-            desiredVelocity = (forward * playerInput.y + right * playerInput.x) * maxSpeed;
+            desiredVelocity = playerInputSpace.TransformDirection(playerInput.x, 0f, playerInput.y) * maxSpeed;
         }
 
         else
         {
-           desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+            desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
         }
 
-        Vector3 displacement = velocity * Time.deltaTime;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
 
-        transform.localPosition += displacement;
+        Vector3 displacement = velocity * Time.deltaTime;
+        Vector3 newPosition = transform.localPosition + displacement;
+        transform.localPosition = newPosition;
     }
 }
